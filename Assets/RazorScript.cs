e3dfd0bb984f358ae747af3591ef91371bdbb11f
Nino Verhaegh <nino.verhaegh@gmail.com>
2024-02-11 02:20:24 +0100

    added cutscene

--- Assets/RazorScript.cs
+++ Assets/RazorScript.cs
@@ -1,26 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.VisualScripting.Member;
 
 public class RazorScript : MonoBehaviour, IInteractable
 {
     [SerializeField] private GameObject razorBlade;
     [SerializeField] private Vector3 shownBladeEulerRotation;
+    [SerializeField] private AudioClip showBladeClip;
+    [SerializeField] private Dialogue firstCuttingDialogue;
+    [SerializeField] private AudioClip cutBladeClip;
     public float lerpSpeed;
     public float showBladeDelay;
     private bool bladeShowing;
     private PlayerInputActions playerInputActions;
+    private bool isCutting;
+    private Transform childRazorTransform;
+    private Vector3 downPosition;
+    private AudioSource source;
     // Start is called before the first frame update
     void Start()
     {
+        source = GetComponent<AudioSource>();
         playerInputActions = new PlayerInputActions();
         playerInputActions.Enable();
+        childRazorTransform = transform.GetChild(0);
+        downPosition = childRazorTransform.localPosition + Vector3.down;
     }
 
     // Update is called once per frame
     void Update()
     {
         LerpBladeRotation(bladeShowing);
+        if (isCutting)
+        {
+            LerpDown();
+        }
+    }
+
+    private void LerpDown()
+    {
+        childRazorTransform.localPosition = Vector3.Lerp(childRazorTransform.localPosition, downPosition, lerpSpeed * Time.deltaTime);
     }
 
     public void ShowBlade()
@@ -48,13 +68,21 @@ public class RazorScript : MonoBehaviour, IInteractable
 
     void IInteractable.Interact()
     {
-        StartCoroutine(ShowBladeDelay(showBladeDelay));
+        StartCoroutine(ShowBladeAnimation(showBladeDelay));
     }
 
-    private IEnumerator ShowBladeDelay(float delay)
+    private IEnumerator ShowBladeAnimation(float delay)
     {
         yield return new WaitForSeconds(delay);
         ShowBlade();
+        source.PlayOneShot(showBladeClip);
+        yield return new WaitForSeconds(delay);
+        DialogueManager.instance.StartDialogue(firstCuttingDialogue);
+        yield return new WaitForSeconds(7);
+        isCutting = true;
+        yield return new WaitForSeconds(delay);
+        source.PlayOneShot(cutBladeClip);
+
     }
 
     //Replace Vector3.Lerp because that one cant cross an angle of 0, so for instance lerp from 10 to -10 is only possible with this method without weird behaviour
